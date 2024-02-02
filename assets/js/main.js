6d6e49cba916c2fca7ae632e5325a4328b8a1e95
Enéas Marques <eneasmarques@outlook.com.br>
2024-02-02 17:21:38 -0300

    feat: implement dynamic pokemon creation with pokeapi
    - Integrated PokeAPI to fetch Pokémon data dynamically.
    - Implemented logic to create Pokémon cards based on fetched data.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
-window.onload =  () => {
-    const cards = document.querySelectorAll(".pokemon");
+const pokemonList = document.getElementById("pokemonList");
 
-    cards.forEach(card => {
-        const pokemonType = card.querySelector(".type").textContent.trim();
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML = pokemons.map(convertPokemonListToLi).join("");
+  setPokemonBackgroundColor();
+});
 
-        const color = getComputedStyle(card).getPropertyValue(`--color-${pokemonType}`);
-        
-        card.style.backgroundColor = color;
-    });
+function convertPokemonTypesToLi(pokemonTypes = []) {
+  return pokemonTypes.map(
+    (slotType) => `<li class="type">${slotType.type.name}</li>`
+  );
+}
 
-};
+function convertPokemonListToLi(pokemon) {
+  return `
+        <li class="pokemon ">
+            <span class="number">#${pokemon.order
+              .toString()
+              .padStart(3, "0")}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+            <ol class="types">
+                ${convertPokemonTypesToLi(pokemon.types).join("")}
+            </ol>
+            <img src="${pokemon.sprites.other.dream_world.front_default}"
+                alt="${pokemon.name}" />
+            </div>
+        </li>
+    `;
+}
+
+function setPokemonBackgroundColor() {
+  const pokemonCards = document.querySelectorAll(".pokemon");
+  pokemonCards.forEach((pokemonCard) => {
+    const pokemonType = pokemonCard.querySelector(".type").textContent.trim();
+    const backgroundColor = getComputedStyle(pokemonCard).getPropertyValue(
+      `--color-${pokemonType}`
+    );
+    pokemonCard.style.backgroundColor = backgroundColor;
+  });
+}
